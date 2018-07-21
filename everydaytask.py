db2d03ea305a6aaa89f4dbd30e1ee462b4573ee5
Strogera <Strogera@bitbucket.org>
2018-07-21 17:39:03 +0300

    clean up code

--- everydaytask.py
+++ everydaytask.py
@@ -3,6 +3,7 @@ import sys
 
 
 def setCheckBoxColour(cb):
+    #changes the colour of the checkbox to gray when ticked and black otherwise
     if todaysCheckBoxes[cb]:
         app.setCheckBoxFg(cb, "#808080")
         app.setCheckBoxActiveFg(cb, "#808080")
@@ -12,6 +13,7 @@ def setCheckBoxColour(cb):
 
 
 def checkBoxTicked(cb):
+    #when a check box is ticked we need to update the global stored values of the checkboxes
     if todaysCheckBoxes[cb]:
         todaysCheckBoxes[cb]=False
     else:
@@ -19,109 +21,104 @@ def checkBoxTicked(cb):
     setCheckBoxColour(cb)
 
 def editButton(btn):
+    #the main functionlity of this button is to switch between the checkboxes and the entries 
+    #and connect them so that the entries edit the contents of the checkboxes
     if btn=="Done":
+        #then user is done editing the entries and he needs to see the updated checkboxes
         count=0
         dic=app.getAllEntries()
+        #writing the changes on the file is not entirely needed since its done on exit either way
+        #it may be useful but if its a permormance problem later on i can remove it
         with app.labelFrame("Today's Goals"):
             with open(".TodayLogFile", "w+") as curFile:
                 curFile.write("#Deleting this file you wont be able to track your todays goals. \n#Its recommended that you don't edit it manually either, so there is no data loss. \n#The program will read the first 3 non-comment lines.\n")
                 for textAreaName in dic:
                     if "Task" in textAreaName:
+                        #write to file
                         if todaysCheckBoxes[textAreaName]:
                             curFile.write('1')
                         else:
                             curFile.write('0')
                         curFile.write(dic[textAreaName]+"\n")
+                        #update the memory copy of the checkboxes
                         todaysCheckBoxesContent[textAreaName]=dic[textAreaName]
+                        #hide the entry
                         app.hideEntry(textAreaName)
+                        #add the updated checkbox
                         app.addNamedCheckBox(dic[textAreaName],textAreaName, count, 0)
+                        #update its ticked state
                         if todaysCheckBoxes[textAreaName]:
                             app.setCheckBox(textAreaName, ticked=True, callFunction=False)
                         else:
                             app.setCheckBox(textAreaName, ticked=False, callFunction=False)
+                        #on click use the checkBoxTicked function 
                         app.setCheckBoxChangeFunction(textAreaName, checkBoxTicked)
+                        #set the correct colour
                         setCheckBoxColour(textAreaName)
                         count+=1
             app.hideButton("Done")
             app.showButton("Edit")
     elif btn=="Edit":
+        #then the user wants to edit the contents of the checkboxes
+        #and needs to see the entries
         dic=app.getAllCheckBoxes()
         count=1
         with app.labelFrame("Today's Goals"):
-            for textBoxName in dic:
-                if "Task" in textBoxName:
-                    todaysCheckBoxes[textBoxName]=app.getCheckBox(textBoxName)
-                    app.showEntry(textBoxName)
-                    app.setEntry(textBoxName, todaysCheckBoxesContent[textBoxName])
-                    app.removeCheckBox(textBoxName)
+            for checkBoxName in dic:
+                if "Task" in checkBoxName:
+                    #show the entry and edit its content with the content of the
+                    #checkbox respectively
+                    app.showEntry(checkBoxName)
+                    app.setEntry(checkBoxName, todaysCheckBoxesContent[checkBoxName])
+                    #I need to remove the checkboxes and make them again when needed
+                    #because the appJar library doesn't provide
+                    #a way to set a namedCheckBox's content
+                    app.removeCheckBox(checkBoxName)
                     count+=1
             app.hideButton("Edit")
             app.showButton("Done")
 
-def editView():
-    app.setSticky("ew")
-    app.addEntry("Task1", 0, 0)
-    app.addEntry("Task2", 1, 0)
-    app.addEntry("Task3", 2, 0)
-    app.addButton("Done", editButton, 3, 0)
-    app.addButton("Edit", editButton, 3, 0)
-    app.hideButton("Edit")
+def addEntriesElements(listOfEntries):
+    #adds as much entries elements as there are in the given list with the name provided
+    #ex: ["Task1", "Task2"]
+    #adds two Entries elements with the respective names
+    count=0
+    for entry in listOfEntries:
+        app.addEntry(entry, count, 0)
+        count+=1
 
-def hideEditView():
-    app.hideEntry("Task1")
-    app.hideEntry("Task2")
-    app.hideEntry("Task3")
-    app.hideButton("Done")
-    app.showButton("Edit")
+def hideEntriesElements(listOfEntries):
+    #hides all the entry elements found in the given list
+    for entry in listOfEntries:
+        app.hideEntry(entry)
 
-def showView():
-    with open(".TodayLogFile", "r") as todaysFile:
-        line=todaysFile.readline()
-        if line:
-            while  line[0]=='#':
-                line=todaysFile.readline()
-                if not line:
-                   editView() 
-                   return
-        if line[0]=='1':
-            todaysCheckBoxes["Task1"]=True
-        todaysCheckBoxesContent["Task1"]=line[1:-1]
-        app.addNamedCheckBox(todaysCheckBoxesContent["Task1"],"Task1", 0, 0)
-        app.setCheckBox("Task1", ticked=todaysCheckBoxes["Task1"])
-        app.setCheckBoxChangeFunction("Task1", checkBoxTicked)
-        setCheckBoxColour("Task1")
-        line=todaysFile.readline()
-        if line:
-            if line[0]=='1':
-                todaysCheckBoxes["Task2"]=True
-            todaysCheckBoxesContent["Task2"]=line[1:-1]
-            app.addNamedCheckBox(todaysCheckBoxesContent["Task2"],"Task2", 1, 0)
-            app.setCheckBox("Task2", ticked=todaysCheckBoxes["Task2"])
-            app.setCheckBoxChangeFunction("Task2", checkBoxTicked)
-            setCheckBoxColour("Task2")
-        else:
-            todaysCheckBoxesContent["Task2"]=""
-            app.addNamedCheckBox("", "Task2", 1, 0)
-            app.setCheckBoxChangeFunction("Task2", checkBoxTicked)
+def addCheckBoxesElementsFromFile(listOfCheckBoxes, inputFile):
+    #adds as much checkboxes as there are in the given list with the name provided
+    #and with the content found in the local file
+    with open(inputFile, "r") as todaysFile:
+        count=0
+        for checkBox in listOfCheckBoxes:
+            line=todaysFile.readline()
+            if line:
+                #ignore comment lines
+                while  line[0]=='#':
+                    line=todaysFile.readline()
+                if line[0]=='1':
+                    todaysCheckBoxes[checkBox]=True
+                todaysCheckBoxesContent[checkBox]=line[1:-1]
+                app.addNamedCheckBox(todaysCheckBoxesContent[checkBox],checkBox, count, 0)
+                app.setCheckBox(checkBox, ticked=todaysCheckBoxes[checkBox])
+                app.setCheckBoxChangeFunction(checkBox, checkBoxTicked)
+                setCheckBoxColour(checkBox)
 
-        line=todaysFile.readline()
-        if line:
-            if line[0]=='1':
-                todaysCheckBoxes["Task3"]=True
-            todaysCheckBoxesContent["Task3"]=line[1:-1]
-            app.addNamedCheckBox(todaysCheckBoxesContent["Task3"],"Task3", 2, 0)
-            app.setCheckBox("Task3", ticked=todaysCheckBoxes["Task3"])
-            app.setCheckBoxChangeFunction("Task3", checkBoxTicked)
-            setCheckBoxColour("Task3")
-        else:
-            todaysCheckBoxesContent["Task3"]=""
-            app.addNamedCheckBox("", "Task3", 2, 0)
-        app.setCheckBoxChangeFunction("Task3", checkBoxTicked)
-        print todaysCheckBoxesContent
-        editView()
-        hideEditView()
+            else:
+                todaysCheckBoxesContent[checkBox]=""
+                app.addNamedCheckBox("", checkBox, count, 0)
+                app.setCheckBoxChangeFunction(checkBox, checkBoxTicked)
+            count+=1
 
 def saveChanges():
+    #save any changes localy so they are available when the program is opened again
     with open(".TodayLogFile", "w+") as curFile:
         curFile.write("#Deleting this file you wont be able to track your todays goals. \n#Its recommended that you don't edit it manually either, so there is no data loss. \n#The program will read the first 3 non-comment lines.\n")
         for task in todaysCheckBoxes:
@@ -132,22 +129,36 @@ def saveChanges():
             curFile.write(todaysCheckBoxesContent[task]+"\n")
     return True
 
+
+
 with gui("tba") as app:
     app.setBg("lightblue")
     app.setSize(600, 300)
     with app.tabbedFrame("pages"):
         with app.tab("Today"):
+            #local copy of the contents in order to manage them, present them and keep them up to date
             todaysCheckBoxes={"Task1":False, "Task2":False, "Task3":False}
             todaysCheckBoxesContent={"Task1":"", "Task2":"", "Task3":""}
+            tasks=["Task1", "Task2", "Task3"]
             with app.labelFrame("Today's Goals"):
+                app.setSticky("ew")
                 try:
-                    showView()
+                    #if the program has stored data show them
+                    addCheckBoxesElementsFromFile(tasks, ".TodayLogFile")
+                    addEntriesElements(tasks)
+                    hideEntriesElements(tasks)
+                    app.addButton("Done", editButton, len(tasks), 0)
+                    app.addButton("Edit", editButton, len(tasks), 0)
+                    app.hideButton("Done")
                 except IOError:
-                    editView() 
-                
-                
-
-
+                    #else make them
+                    addEntriesElements(tasks) 
+                    app.addButton("Done", editButton, len(tasks), 0)
+                    app.addButton("Edit", editButton, len(tasks), 0)
+                    app.hideButton("Edit")
+        with app.tab("Tomorrow"):
+            with app.labelFrame("Tomorrow's Goals"):
+                app.addEntry("asdf")
         with app.tab("Long Term"):
             with app.labelFrame("Long Term Goals"):
                 app.addEntry("Goals")
